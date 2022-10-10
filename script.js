504de0f908a8e0d7a9d51ca489e5aa83fc08df66
Simone <simonebackesv@gmail.com>
2022-10-10 23:09:34 +0100

    exercise 10, 11 and 13

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ function changeBackground() {
 function changeFooter() {
   document.getElementById("footer").innerHTML = "any adress - baker street 221";
 }
+
+function addClass() {
+  let list = document.querySelectorAll("p");
+  for (let i = 0; i < list.length; i++) {
+    list[i].classList.add("newClass");
+  }
+  // document.querySelector("p").classList.add("newClass"); // olny works for de first p tag
+}
+
+function toggleStyle() {
+  let image = document.querySelectorAll("img");
+  for (let i = 0; i < image.length; i++) {
+    const element = image[i];
+    element.classList.toggle("hidden");
+  }
+}
+
+function colorPrices() {
+  let color =
+    "#" +
+    Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0")
+      .toUpperCase();
+
+  console.log(color);
+  let prices = document.querySelectorAll("h4");
+  for (let i = 0; i < prices.length; i++) {
+    const element = prices[i];
+    element.style.color = color;
+  }
+}
